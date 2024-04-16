840ccb66032d8827537ab78b5a50f6e153f0b885
dpsk3048 <dpsk3048@naver.com>
2024-04-17 07:08:04 +0900

    관리자페이지 디폴트 레이아웃 나누기, 컨트롤러 생성

--- src/main/java/kr/co/lotteon/controller/admin/AdminConfigController.java
+++ src/main/java/kr/co/lotteon/controller/admin/AdminConfigController.java
@@ -6,32 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
-public class AdminController {
+public class AdminConfigController {
+
 
     @GetMapping(value = {"/admin", "/admin/index"})
     public String index(){
         return "/admin/index";
-    }
-
-    @GetMapping("/admin/config/banner")
-    public String banner(){
-        return "/admin/config/banner";
-    }
-
-    @GetMapping("/admin/config/info")
-    public String info(){
-        return "/admin/config/info";
-    }
-
-
-    @GetMapping("/admin/product/list")
-    public String list(){
-        return "/admin/product/list";
-    }
-
-    @GetMapping("/admin/product/register")
-    public String register(){
-        return "/admin/product/register";
     }
 
     @GetMapping("/admin/charts")
@@ -51,16 +31,16 @@ public class AdminController {
     }
 
 
+    @GetMapping("/admin/config/banner")
+    public String banner(){
+
+        return "/admin/config/banner";
+    }
+
+    @GetMapping("/admin/config/info")
+    public String info(){
+
+        return "/admin/config/info";
+    }
 
 }
-
-
-
-
-
-
-
-
-
-
-
