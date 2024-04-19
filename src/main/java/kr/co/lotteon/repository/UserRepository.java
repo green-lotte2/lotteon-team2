6bad00ed5ad10702c5d7f084b15370775a56dea9
rhdyeji12 <rhdyeji12@gmail.com>
2024-04-19 12:01:32 +0900

    회원가입 우편번호 추가

--- src/main/java/kr/co/lotteon/repository/UserRepository.java
+++ src/main/java/kr/co/lotteon/repository/UserRepository.java
@@ -4,8 +4,9 @@ import kr.co.lotteon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    boolean existsByUid(String uid);
+    boolean existsByEmail(String email);
+    boolean existsByHp(String hp);
 
-    int countByUid(String uid);
-    int countByHp(String hp);
-    int countByEmail(String email);
+
 }
--- src/main/java/kr/co/lotteon/service/UserService.java
+++ src/main/java/kr/co/lotteon/service/UserService.java
@@ -1,5 +1,9 @@
 package kr.co.lotteon.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpSession;
 import kr.co.lotteon.dto.UserDTO;
 import kr.co.lotteon.entity.User;
 import kr.co.lotteon.entity.UserDetail;
@@ -8,13 +12,13 @@ import kr.co.lotteon.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Log4j2
 @Service
@@ -25,6 +29,7 @@ public class UserService {
     private final UserDetailRepository userDetailRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private JavaMailSender javaMailSender;
 
     @Transactional
     public void insertUser(UserDTO userDTO) {
@@ -47,18 +52,50 @@ public class UserService {
 
     }
 
-    public int countById(String uid){
-        return userRepository.countByUid(uid);
+    public boolean existsById(String uid) {
+        return userRepository.existsByUid(uid);
     }
 
-     public int countByHp(String hp){
-        return userRepository.countByHp(hp);
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
     }
 
-    public int countByEmail(String email){
-        return userRepository.countByEmail(email);
+    public boolean existsByHp(String hp) {
+        return userRepository.existsByHp(hp);
     }
 
+    @Value("${spring.mail.username}")
+    private String sender;
+
+    public void sendEmailCode(HttpSession session, String receiver){
+
+        log.info("sender : " + sender);
+
+        // MimeMessage 생성
+        MimeMessage message = javaMailSender.createMimeMessage();
+
+        // 인증코드 생성 후 세션 저장
+        int code = ThreadLocalRandom.current().nextInt(100000, 1000000);
+        session.setAttribute("code", String.valueOf(code));
+
+        log.info("code : " + code);
+
+        String title = "lotteon 인증코드 입니다.";
+        String content = "<h1>인증코드는 " + code + "입니다.</h1>";
+
+        try {
+            message.setFrom(new InternetAddress(sender, "보내는 사람", "UTF-8"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            message.setSubject(title);
+            message.setContent(content, "text/html;charset=UTF-8");
+
+            javaMailSender.send(message);
+
+        }catch(Exception e){
+            log.error("sendEmailConde : " + e.getMessage());
+        }
+
+    }
 
 
 
