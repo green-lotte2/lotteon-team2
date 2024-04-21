d52fdcaa6904c2b257b4fbb9b9e29f5a69ea6b6f
dpsk3048 <dpsk3048@naver.com>
2024-04-22 07:58:16 +0900

    어드민 공지사항, 자주묻는질문 리스트 출력& 글보기

--- src/main/java/kr/co/lotteon/mapper/AdminMapper.java
+++ src/main/java/kr/co/lotteon/mapper/AdminMapper.java
@@ -1,5 +1,6 @@
 package kr.co.lotteon.mapper;
 
+import kr.co.lotteon.dto.FaqDTO;
 import kr.co.lotteon.dto.NoticeDTO;
 import kr.co.lotteon.dto.ProductDTO;
 import org.apache.ibatis.annotations.Mapper;
@@ -12,13 +13,30 @@ public interface AdminMapper{
 
     public List<UserDTO> adminSelectUsers();
 
+    //🎈상품
     // admin 상품목록
     public List<ProductDTO> adminSelectProducts();
-
     // admin 상품삭제
     public void adminDeleteProduct(int pno);
 
-    // cs modify용
-    public void updateAdminNotice(NoticeDTO dto);
+    
+    //🎈공지사항//
+    //  목록
+    public List<NoticeDTO> adminNoticeList(int start);
+    //  view
+    public NoticeDTO adminNoticeView(int noticeno);
+    //  수정
+    public void adminNoticeUpdate(NoticeDTO dto);
+
+
+
+    //🎈자주묻는질문//
+    // 목록
+    public List<FaqDTO> adminFaqList(int cate1);
+    // view
+    public FaqDTO adminFaqView(int faqno);
+
+
+
 
 }
