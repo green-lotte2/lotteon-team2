db0210cd5631ae7dfce62ba65a61f1a8fc4b1549
kkome <exazunyan1@gmail.com>
2024-04-26 12:07:14 +0900

    4월26일 아이디기억하기 및 찾기기능 추가

--- src/main/java/kr/co/lotteon/repository/UserRepository.java
+++ src/main/java/kr/co/lotteon/repository/UserRepository.java
@@ -3,10 +3,14 @@ package kr.co.lotteon.repository;
 import kr.co.lotteon.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, String> {
     boolean existsByUid(String uid);
     boolean existsByEmail(String email);
     boolean existsByHp(String hp);
 
+    // 이름과 이메일 찾기 (아이디 찾기용)
+    public Optional<User> findUserByNameAndEmail(String name, String email);
 
 }
